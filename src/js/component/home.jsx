4a9b2498629c694489be72cb10ee7c990e912d02
lauramagallanes <pasocenturiontours@gmail.com>
2022-12-11 20:47:50 +0000

    actualizacion del codigo para deploy

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,86 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	// const [song, setSong] = useState(null);
+	const [listaSongs, setListaSongs] = useState([]); //listaSongs es mi array donde tengo cada cancion
+	let startSong = useRef(null);
+	console.log(startSong);
+	// let i=0;
+	let [positionSong, setPositionSong] = useState(0);
+
+	function getSongs() {
+		fetch("https://assets.breatheco.de/apis/sound/songs")
+			.then((response) => {
+				console.log(response.status);
+				return response.json()
+			}) //promesa 1
+			.then((data) => {
+				console.log(data);
+				return setListaSongs(data) // asi completo mi array listaSongs con lo que fetcheie
+			})
+			.catch((err) => console.log(err))
+
+	}
+// let startSong = ""
+	function reproducir(url, id){
+
+		if(startSong.current.paused){
+			console.log("estoy en el if");
+			startSong.current.src = `https://assets.breatheco.de/apis/sound/${url}`;
+			startSong.current.play();
+		}else{
+			startSong.current.pause();
+			console.log("Esta en play");
+
+		}
+		
+		
+		setPositionSong(id-1);
+		
+						
+	}
+
+	function forward(){
+		setPositionSong(positionSong++);
+		startSong.current.src = `https://assets.breatheco.de/apis/sound/${listaSongs[positionSong].url}`;
+		console.log(startSong.current.src);
+		startSong.current.play()
+	}
+
+	function backward(){
+		setPositionSong(positionSong--)
+		startSong.current.src = `https://assets.breatheco.de/apis/sound/${listaSongs[positionSong].url}`;
+		console.log(startSong.current.src);
+		startSong.current.play()
+	}
+		
+	useEffect(() => {
+		getSongs();
+				
+	}, [])
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container bg-dark">
+			<div className="row">
+				
+					{listaSongs.map((item) => <button className="btn btn-dark text-start" type="button" key={item.id} onClick={()=> reproducir(item.url, item.id)}>{item.id} ---- {item.name}</button>)}
+				
+			</div>
+
+			<div className="container d-flex justify-content-center bg-dark">
+			<button type="button" className="btn btn-info btn-lg" onClick={() => backward(positionSong)}><i className="fa fa-backward"></i></button><audio ref={startSong} id="reproductor" controls>				
+					<source src={startSong} type="audio/mpeg" />
+					Your browser does not support the audio tag.
+				</audio><button type="button" className="btn btn-info btn-lg" onClick={() => forward(positionSong)}><i className="fa fa-forward"></i></button>
+			</div>
+			
 		</div>
 	);
 };
 
+// onClick={() => cancionReproducir(item.id)}
 export default Home;
